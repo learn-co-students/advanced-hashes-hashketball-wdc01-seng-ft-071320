9931b89478fe7727922f1d178f59f44b02d56c9b
Jina Strickland <jina30@gmail.com>
2020-07-06 16:38:13 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -204,9 +204,13 @@ end
 
 
 def big_shoe_rebounds
+  shoe_size = 0
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
-      player[:shoe].max_by {|k, v| v}
+binding.pry
+      player[:shoe].each_value > shoe_size
+binding.pry
+      shoe_size = player[:shoe].each_value
     binding.pry
        end
     end
